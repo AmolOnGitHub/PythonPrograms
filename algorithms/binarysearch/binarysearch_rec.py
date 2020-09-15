c131b52a8cb61fe87662b9b79fc76b71e7000dba
AmolOnGitHub <63136382+AmolOnGitHub@users.noreply.github.com>
2020-09-15 10:26:30 +0530

    Bug fixes in binary search
    Also binary search for string

--- algorithms/binarysearch/binarysearch_rec.py
+++ algorithms/binarysearch/binarysearch_rec.py
@@ -3,19 +3,23 @@ import math as math
 def middle(begin, end):
     return math.floor((begin + end) / 2)
 
-def binary(arr, toSearch, s, e):
+def binary(arr, toSearch, s, e, c):
+    if c >= len(arr):
+        mid = -1        
+        return -1
+
     mid = middle(s, e) 
     if toSearch == int(arr[mid]):
         return mid
     elif toSearch < int(arr[mid]):
-        return binary(arr, toSearch, s, mid)
+        return binary(arr, toSearch, s, mid, c + 1)
     elif toSearch > int(arr[mid]):
-        return binary(arr, toSearch, mid + 1, e)
+        return binary(arr, toSearch, mid + 1, e, c + 1)
 
 
 arr = input().split(", ")
 toSearch = int(input())
 length = len(arr)
 
-index = binary(arr, toSearch, 0, length - 1)
+index = binary(arr, toSearch, 0, length - 1, 0)
 print(toSearch, "is at index:", index)
--- algorithms/binarysearch/binarysearch_str.py
+++ algorithms/binarysearch/binarysearch_str.py
@@ -4,23 +4,25 @@ def middle(begin, end):
     return math.floor((begin + end) / 2)
 
 arr = input().split(", ")
-toSearch = int(input())
+toSearch = input()
 length = len(arr) - 1
 
 s = 0
 e = length
 mid = 0
 c = 0
-
-while not (int(arr[mid]) == toSearch):
+while not (arr[mid] == toSearch):
     if c >= length:
         mid = -1        
         break
     
     mid = middle(s, e)
-    if int(arr[mid]) > toSearch:
-        e = mid
-    elif int(arr[mid]) < toSearch:
+    if arr[mid] > toSearch:
+        e = mid - 1
+    elif arr[mid] < toSearch:
         s = mid + 1
+    c += 1
 
 print(toSearch, "is at index:", mid)
+if mid == -1:
+    print("The element", toSearch, "is not present in the list of elements provided.")
