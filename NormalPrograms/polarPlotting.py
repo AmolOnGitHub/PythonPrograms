f0372a1002d19694474db65e0f2def501a9c83e6
Amol <amolvijayachandran@gmail.com>
2021-07-04 20:24:40 +0530

    Polar prime plotting

---
+++ NormalPrograms/polarPlotting.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import math
+
+def is_prime(n):
+    if n == 1:
+        return False
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+def toCart(r, theta):
+    x = r * math.cos(theta)
+    y = r * math.sin(theta)
+    return (x, y)
+
+xP = []
+yP = []
+
+for i in range(1000000):
+    if is_prime(i):
+        x, y = toCart(i, i)
+        xP.append(x)
+        yP.append(y)
+
+plt.plot(xP, yP, ".")
+plt.show()
--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-a = [5, 2]
-print(a[::-1])
+
+k = int(input())
+cases = [input() for _ in range(k)]
+for s in cases:
+    if s.isnumeric():
+        pass
+    elif not s.isnumeric():
+        print(s[::2], s[1::2])
+        k += 1
+
