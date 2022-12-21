0f3d4aad2449419116838d8ea85186777b64a02b
Amol <amolvijayachandran@gmail.com>
2022-12-21 16:27:50 +0530

    working dist check

--- Random/sierpinski.py
+++ Random/sierpinski.py
@@ -40,34 +40,16 @@ for i in range(SIDES - 2):
 xpts = [pt[0] for pt in vertices]
 ypts = [pt[1] for pt in vertices]
 
+maxdist = dist(vertices[0][0], vertices[0][1], vertices[int(SIDES/2)][0], vertices[int(SIDES/2)][1]) - 2
+eligible = False
+while not eligible: 
+    randpt = (random.uniform(min(xpts), max(xpts)), random.uniform(min(ypts), max(ypts)))
+    dists = [dist(randpt[0], randpt[1], v[0], v[1]) for v in vertices]
+    eligible = True
+    for dist in dists:
+        if dist > maxdist: eligible = False
 
-
-
-equations = []
-
-for i in range(len(vertices) - 1):
-    x1 = vertices[i][0]
-    y1 = vertices[i][1]
-    x2 = vertices[i + 1][0]
-    y2 = vertices[i + 1][1]
-    equations.append(geteq(x1, y1, x2, y2))
-
-equations.append(geteq(vertices[0][0], vertices[0][1], x2, y2))
-
-randpt = (random.uniform(min(xpts), max(xpts)), random.uniform(min(ypts), max(ypts)))
-randpt = (-10, 5)
-
-req = geteq(randpt[0], randpt[1], max(xpts) + 10, randpt[1])
-c = 0
-for eqn in equations:
-    c += len(solve((eqn, req), (xc, yc)))
-
-dists = [dist(randpt[0], randpt[1], v[0], v[1]) for v in vertices]
-maxdist = dist(vertices[0][0], vertices[0][1], vertices[int(SIDES/2)][0], vertices[int(SIDES/2)][1])
-print(c)
-print(randpt)
-
-rand_pts = []
+rand_pts = [rantpt]
 
 
 
